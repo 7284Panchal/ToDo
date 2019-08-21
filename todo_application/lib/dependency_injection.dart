2b46123469a89c413a0bfc847f9a6bd6764989c9
Mayank <7284panchal@gmail.com>
2019-08-22 03:08:57 +0530

    Added ViewModel and Model with functionality
    Added
    1. Model for todo list,
    2. Preference Helper to store todo list,
    3. Todo data service to load and set todo list data, |
    4. View Model to perform add, update, delete, read todo list.
    5. Dependency injection to inject dependences

---
+++ todo_application/lib/dependency_injection.dart
@@ -0,0 +1,41 @@
+import 'package:flutter_simple_dependency_injection/injector.dart';
+import 'package:todo_application/data/preference_helper.dart';
+import 'package:todo_application/data_service/todo_data_service.dart';
+import 'package:todo_application/view_models/todo_view_model.dart';
+
+class DependencyInjection {
+  static final DependencyInjection _dependencyInjection =
+      DependencyInjection._internal();
+
+  factory DependencyInjection() {
+    return _dependencyInjection;
+  }
+
+  DependencyInjection._internal();
+
+  void configureDependency() {
+    Injector injector = Injector.getInjector();
+
+    //preference_helper
+    injector.map<IPreferenceHelper>(
+      (i) => PreferenceHelper(),
+      isSingleton: true,
+    );
+
+    //todo_data_service
+    injector.map<ITodoDataService>(
+      (i) => TodoDataService(
+            iPreferenceHelper: i.get<IPreferenceHelper>(),
+          ),
+      isSingleton: true,
+    );
+
+    //todo_view_model
+    injector.map<ITodoViewModel>(
+      (i) => TodoViewModel(
+            todoDataService: i.get<ITodoDataService>(),
+          ),
+      isSingleton: true,
+    );
+  }
+}
---
+++ todo_application/lib/view_models/todo_view_model.dart
@@ -0,0 +1,179 @@
+import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+import 'package:todo_application/data_service/todo_data_service.dart';
+import 'package:todo_application/models/todo_list.dart';
+
+abstract class ITodoViewModel {
+  bool isLoading();
+
+  String getMessage();
+
+  int getTodoListCount();
+
+  TodoList getTodoList();
+
+  void loadTodoList();
+
+  void addTask({
+    String task,
+    String description,
+    VoidCallback onComplete,
+    VoidCallback onError,
+  });
+
+  void updateTask({
+    int id,
+    String task,
+    String description,
+    bool isCompleted,
+    VoidCallback onComplete,
+    VoidCallback onError,
+  });
+
+  void deleteTask({
+    int id,
+    VoidCallback onComplete,
+    VoidCallback onError,
+  });
+}
+
+class TodoViewModel extends Model with ITodoViewModel {
+  ITodoDataService todoDataService;
+
+  TodoViewModel({@required this.todoDataService});
+
+  bool _isLoading;
+
+  String _message;
+
+  TodoList _todoList;
+
+  @override
+  bool isLoading() {
+    return _isLoading;
+  }
+
+  @override
+  String getMessage() {
+    return _message;
+  }
+
+  @override
+  int getTodoListCount() {
+    return _todoList.todoItems.length;
+  }
+
+  @override
+  TodoList getTodoList() {
+    return _todoList;
+  }
+
+  @override
+  void loadTodoList() async {
+    _isLoading = true;
+    notifyListeners();
+
+    _todoList = await todoDataService.geTodoList();
+    _message = "You have ${_todoList.todoItems.length} Task";
+
+    _isLoading = false;
+    notifyListeners();
+  }
+
+  @override
+  void addTask({
+    String task,
+    String description,
+    VoidCallback onComplete,
+    VoidCallback onError,
+  }) async {
+    _isLoading = true;
+    notifyListeners();
+
+    //get unique id for todoItem
+    int id = DateTime.now().millisecondsSinceEpoch;
+
+    //create todoItem for the data
+    TodoItem todoItem = TodoItem(
+        id: id, task: task, description: description, isCompleted: false);
+
+    //add todoItem in to todoList
+    _todoList.todoItems.add(todoItem);
+
+    bool result = await todoDataService.setTodoList(todoList: _todoList);
+
+    if (result) {
+      _message = "Task added successfully";
+      _isLoading = false;
+      notifyListeners();
+      onComplete();
+    } else {
+      _message = "Something went wrong.Try again";
+      _isLoading = false;
+      notifyListeners();
+      onError();
+    }
+  }
+
+  @override
+  void updateTask({
+    int id,
+    String task,
+    String description,
+    bool isCompleted,
+    VoidCallback onComplete,
+    VoidCallback onError,
+  }) async {
+    _isLoading = true;
+    notifyListeners();
+
+    //get index of todoItem with matching id
+    int index = _todoList.todoItems.indexWhere((todoItem) => todoItem.id == id);
+
+    //update todoItem with new data
+    _todoList.todoItems.elementAt(index).task = task;
+    _todoList.todoItems.elementAt(index).description = description;
+    _todoList.todoItems.elementAt(index).isCompleted = isCompleted;
+
+    bool result = await todoDataService.setTodoList(todoList: _todoList);
+
+    if (result) {
+      _message = "Task updated successfully";
+      _isLoading = false;
+      notifyListeners();
+      onComplete();
+    } else {
+      _message = "Something went wrong.Try again";
+      _isLoading = false;
+      notifyListeners();
+      onError();
+    }
+  }
+
+  @override
+  void deleteTask({
+    int id,
+    VoidCallback onComplete,
+    VoidCallback onError,
+  }) async {
+    _isLoading = true;
+    notifyListeners();
+
+    //remove todoItem with matching id
+    _todoList.todoItems.removeWhere((todoItem) => todoItem.id == id);
+
+    bool result = await todoDataService.setTodoList(todoList: _todoList);
+
+    if (result) {
+      _message = "Task deleted successfully";
+      _isLoading = false;
+      notifyListeners();
+      onComplete();
+    } else {
+      _message = "Something went wrong.Try again";
+      _isLoading = false;
+      notifyListeners();
+      onError();
+    }
+  }
+}
